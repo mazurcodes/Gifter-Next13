63b9022a50dac735a0ecb0e90ee002a2fa09d688
Bartlomiej <trafficap@gmail.com>
2023-02-28 12:20:50 +0100

    NavBar and Footer components in RootLayout

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Footer = () => {
+  return (
+    <footer className="flex justify-center items-center">
+      This is a footer
+    </footer>
+  );
+};
+
+export default Footer;
