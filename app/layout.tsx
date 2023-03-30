dc7c0afb61eb0cf18ae77f291394cb11b4ecc183
Bartlomiej <trafficap@gmail.com>
2023-03-30 13:09:26 +0200

    change tsconfig paths to @/app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import SearchForm from "./components/SearchForm";
+import SearchForm from './components/SearchForm'
 
 export default function Home() {
   return (
     <>
-    
       <main className="flex-auto flex justify-center items-center">
         <SearchForm />
       </main>
     </>
-  );
+  )
 }
