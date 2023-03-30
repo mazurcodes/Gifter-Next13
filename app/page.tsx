a3ce430c7b2fa6ad1da9cc7b0d9288cd2d35858b
Bartlomiej <trafficap@gmail.com>
2023-03-30 14:02:54 +0200

    change paths and prettier semicolons

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import SearchForm from './components/SearchForm'
+import SearchForm from '@/components/SearchForm';
 
 export default function Home() {
   return (
@@ -7,5 +7,5 @@ export default function Home() {
         <SearchForm />
       </main>
     </>
-  )
+  );
 }
