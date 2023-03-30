7db8fc286b938bdd3a20a2098215b474df61f223
Bartlomiej <trafficap@gmail.com>
2023-03-30 14:03:30 +0200

    move components to the folders

--- app/components/SearchForm/index.tsx
+++ app/components/SearchForm/index.tsx
@@ -16,7 +16,7 @@ const SearchForm = () => {
     <form className="flex flex-col" onSubmit={handleSubmit}>
       <h1 className="logo font-black text-9xl text-center">Gifter</h1>
       <label className="text-center">
-        <p className="p-4">Find your friend’s wish list by email</p>
+        <p className="p-4">Find your friend’s wishlist by email</p>
         <input
           className=" w-96 border p-1 px-5 relative"
           type="email"
