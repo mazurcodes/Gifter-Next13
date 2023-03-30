3db682ddb9703761f51a8b9f3052bfbc54e5f0b3
Bartlomiej <trafficap@gmail.com>
2023-03-30 16:52:28 +0200

    style change

--- app/components/NavBar/NavBarMain.tsx
+++ app/components/NavBar/NavBarMain.tsx
@@ -1,17 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import profileIcon from '@/assets/ProfileIcon.svg';
-import searchIcon from '@/assets/SearchIcon.svg';
 
 const NavBarMain = () => {
   return (
-    <nav className="flex justify-end items-center p-3">
+    <nav className="flex justify-end items-center p-3 text-orange-100">
       <ul className="flex items-center">
-        <li className="p-2">
-          <Link href="/">
-            <Image src={searchIcon} alt="profile icon" />
-          </Link>
-        </li>
         <li className="p-2">
           <Link href="/profile">
             <Image src={profileIcon} alt="profile icon" />
