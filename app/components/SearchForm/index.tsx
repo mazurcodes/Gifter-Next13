dde6c1d6707fd4b682a086ebb6559718d5882bb1
Bartlomiej <trafficap@gmail.com>
2023-03-30 14:17:07 +0200

    added label property

--- app/components/SearchForm/index.tsx
+++ app/components/SearchForm/index.tsx
@@ -1,23 +1,25 @@
-'use client'
+'use client';
 
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useState } from 'react';
 
 const SearchForm = () => {
-  const [search, setSearch] = useState<string>('')
+  const [search, setSearch] = useState<string>('');
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value)
-  }
+    setSearch(event.target.value);
+  };
 
   const handleSubmit = (event: React.SyntheticEvent) => {
-    event.preventDefault()
-  }
+    event.preventDefault();
+  };
   return (
     <form className="flex flex-col" onSubmit={handleSubmit}>
       <h1 className="logo font-black text-9xl text-center">Gifter</h1>
-      <label className="text-center">
+      <label className="text-center" htmlFor="search">
         <p className="p-4">Find your friend’s wishlist by email</p>
         <input
+          id="search"
+          name="search"
           className=" w-96 border p-1 px-5 relative"
           type="email"
           value={search}
@@ -26,7 +28,7 @@ const SearchForm = () => {
         />
       </label>
     </form>
-  )
-}
+  );
+};
 
-export default SearchForm
+export default SearchForm;
