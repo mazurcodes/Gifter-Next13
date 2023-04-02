d450bc2cb4ca42f0cb1e23f76087cb8cbf363604
Bartlomiej <trafficap@gmail.com>
2023-04-02 21:39:21 +0200

    metadata for results page

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,12 +1,18 @@
-import GiftList from "@/components/GiftList";
+import GiftList from '@/components/GiftList';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Gifter | Wishlist',
+  description: 'Wishlist for user',
+};
 
 const ResultsPage = () => {
-    return (
-        <div className="flex-1">
-            <h2 className="text-center">Result for bartek@bartekmazur.dev</h2>
-            <GiftList />
-        </div>
-    );
-}
+  return (
+    <div className="flex-1">
+      <h2 className="text-center">Result for bartek@bartekmazur.dev</h2>
+      <GiftList />
+    </div>
+  );
+};
 
 export default ResultsPage;
