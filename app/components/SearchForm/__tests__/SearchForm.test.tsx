26c226a9fda5d96a395426176676c0370975abc1
Bartlomiej <trafficap@gmail.com>
2023-04-16 15:40:52 +0200

    refactor test to use next-router-mock for useRouter functionality

--- app/components/SearchForm/__tests__/SearchForm.test.tsx
+++ app/components/SearchForm/__tests__/SearchForm.test.tsx
@@ -1,16 +1,18 @@
-import { expect, describe, it } from 'vitest';
+import { expect, describe, it, vi } from 'vitest';
 import { render, screen, waitFor, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SearchForm from '..';
+import mockRouter from 'next-router-mock';
 
-const text = 'hello world';
+const text = 'email@example.com';
+
+vi.mock('next/navigation', () => require('next-router-mock'));
 
 describe('SearchForm component', () => {
   render(<SearchForm />);
   const form = within(screen.getByRole('form'));
   const input = within(form.getByLabelText(/search/i));
   const user = userEvent.setup();
-
 
   it('should render form', () => {
     expect(form).toBeDefined();
@@ -35,13 +37,16 @@ describe('SearchForm component', () => {
     await waitFor(() => {
       expect(form.getByDisplayValue(text)).toBeDefined();
     });
-
   });
 
-  it('should submit the form after user click enter', async () => {
+  it('should submit the form after user click enter and redirect to search page with query', async () => {
     await user.keyboard('{enter}');
     await waitFor(() => {
-      expect(form.queryByDisplayValue(text)).toBeNull();
+      expect(mockRouter).toMatchObject({
+        asPath: `/search?email=${encodeURIComponent(text)}`,
+        pathname: '/search',
+        query: { email: text },
+      });
     });
-  })
+  });
 });
