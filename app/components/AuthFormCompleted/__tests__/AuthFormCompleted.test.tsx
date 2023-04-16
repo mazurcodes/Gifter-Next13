a1aa42f9c9f7dee23ba9bcf54e6223bee03d8926
Bartlomiej <trafficap@gmail.com>
2023-04-16 17:02:48 +0200

    test for the AuthFormCompleted component

---
+++ app/components/AuthFormCompleted/__tests__/AuthFormCompleted.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import AuthFormCompleted from '..';
+
+describe('AuthFormCompleted', () => {
+  it('should render the component with the provided email', () => {
+    const email = 'email@example.com';
+
+    render(<AuthFormCompleted email={email} />);
+
+    expect(screen.getByText(/your are signed in as:/i)).toBeDefined();
+    expect(screen.getByText(email)).toBeDefined();
+  });
+});
--- app/components/AuthFormCompleted/index.tsx
+++ app/components/AuthFormCompleted/index.tsx
@@ -2,7 +2,7 @@ const AuthFormCompleted = ({ email }: { email: string | null | undefined }) => {
   return (
     <div className="wrapper-auth-completed">
       <h3>
-        You are sign in as: <span className="text-orange-500">{email}</span>
+        You are signed in as: <span className="text-orange-500">{email}</span>
       </h3>
     </div>
   );
