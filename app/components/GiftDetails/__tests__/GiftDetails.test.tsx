84cf2814826297436ac95baffbbe273a5fe1a231
Bartlomiej <trafficap@gmail.com>
2023-04-20 12:28:38 +0200

    tests for GiftDetails component

---
+++ app/components/GiftDetails/__tests__/GiftDetails.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import Gift from '@/components/Gift';
+import { Category, Occasion, Priority, Status } from '@/constants';
+import GiftDetails from '..';
+
+const dummyName = 'The Dummy Name';
+const dummyNotes = 'dummy note';
+const dummyPrice = '1934';
+const dummyDate = '01.01.2023';
+const dummyEmail = 'some@example.com';
+
+const dummyGift = {
+  id: 6,
+  ownerEmail: dummyEmail,
+  status: Status.AVAILABLE,
+  name: dummyName,
+  priority: Priority.MEDIUM,
+  occasion: Occasion.NONE,
+  category: Category.FOOD,
+  notes: dummyNotes,
+  price: dummyPrice,
+  date: dummyDate,
+  links: [],
+};
+
+describe('Gift component', () => {
+  render(<GiftDetails data={dummyGift} />);
+  it('should render GiftDetails component with dummy data', () => {
+    expect(screen.getByText(/some@example.com/i)).toBeDefined();
+    expect(screen.getByText(Status.AVAILABLE)).toBeDefined();
+    expect(screen.getByText(dummyName)).toBeDefined();
+    expect(screen.getByText(Priority.MEDIUM)).toBeDefined();
+    expect(screen.getByText(Occasion.NONE)).toBeDefined();
+    expect(screen.getByText(Category.FOOD)).toBeDefined();
+    expect(screen.getByText(dummyNotes)).toBeDefined();
+    expect(screen.getByText(dummyPrice)).toBeDefined();
+    expect(screen.getByText(dummyDate)).toBeDefined();
+  });
+});
