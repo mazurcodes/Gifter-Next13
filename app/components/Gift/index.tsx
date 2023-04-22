2954cb99173f6be118a962ad014d4abbe02d6b00
Bartlomiej <trafficap@gmail.com>
2023-04-22 16:58:56 +0200

    Gift click on functionality with router

--- app/components/Gift/index.tsx
+++ app/components/Gift/index.tsx
@@ -1,18 +1,38 @@
 import { GiftDataType } from '@/types';
 import { priorityColor, shortNotes, statusColor } from '@/utils/server';
+import { useRouter } from 'next/navigation';
 
 type GiftProps = {
   data: GiftDataType;
 };
 
 const Gift = ({ data }: GiftProps) => {
-  const { status, name, priority, occasion, category, price, date, notes } =
-    data;
+  const {
+    status,
+    name,
+    priority,
+    occasion,
+    category,
+    price,
+    date,
+    notes,
+    uid,
+  } = data;
+
+  const router = useRouter();
 
   //TODO: date will be in the ISO string format, use utility function to convert.
 
+  const handleClick = () => {
+    const href = uid && '/gift?id=' + encodeURIComponent(uid);
+    href && router.push(href);
+  };
+
   return (
-    <div className="gift-wrapper grid gap-2 grid-cols-[2fr_10fr_repeat(5,_2fr)_3fr] lg:grid-cols-[2fr_10fr_repeat(2,_2fr)] sm:grid-cols-[2fr_5fr] items-center border-b p-4 hover:bg-gray-100">
+    <div
+      onClick={handleClick}
+      className="gift-wrapper grid gap-2 grid-cols-[2fr_10fr_repeat(5,_2fr)_3fr] lg:grid-cols-[2fr_10fr_repeat(2,_2fr)] sm:grid-cols-[2fr_5fr] items-center border-b p-4 hover:bg-gray-100 hover:cursor-pointer"
+    >
       <div
         className={`gift-status text-xs font-semibold ${statusColor(status)}`}
       >
