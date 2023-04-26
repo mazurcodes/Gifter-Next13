3f7227b50aa7ceb67401b684d77d5a5a1792ed97
Bartlomiej <trafficap@gmail.com>
2023-04-26 12:27:40 +0200

    Footer component

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center">
-      This is a footer
+    <footer className="flex justify-between text-sm border-t border-gray-100 p-4 text-gray-400 ">
+      <div>© {new Date().getFullYear()} Gifter</div>
+      <a target="_blank" href="https://bartekmazur.dev">
+        bartekmazur.dev
+      </a>
     </footer>
   );
 };
