8584a92ca150f9301901b4fdd3d4fe5669e1a033
Bartlomiej <trafficap@gmail.com>
2023-05-03 14:50:36 +0200

    remove unwanted console log

--- app/firebase/crudUtils.ts
+++ app/firebase/crudUtils.ts
@@ -52,7 +52,6 @@ export const getAllGifts = async (
   try {
     const q = query(giftsCollection, where('ownerEmail', '==', ownerEmail));
     const snapshot = await getDocs(q);
-    console.log('get all gift fn email:', ownerEmail);
     return snapshot.docs.map(
       (doc) => ({ ...doc.data(), uid: doc.id } as GiftDataType)
     );
