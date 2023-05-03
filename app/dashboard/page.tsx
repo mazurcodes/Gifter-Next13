5d013ed8c5dae707ad5c6d96dd1d58f6af5cc230
Bartlomiej <trafficap@gmail.com>
2023-05-03 11:24:10 +0200

    initial refactor of /dashboard general page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,12 +7,47 @@ const DashboardPage = () => {
   const [user] = useAuthState(auth);
 
   return (
-    <>
-      <h2 className="top-menu-header font-semibold text-lg">
-        Hello,{' '}
-        <span className="font-normal text-orange-500">{user?.email}</span>
-      </h2>
-    </>
+    <div className="dashboard-wrapper">
+      <label className="dashboard-username font-semibold text-lg flex flex-col pb-4">
+        Username: <input className="p-2 border" value="MazurCodes" disabled />
+      </label>
+
+      {/* TODO: this password will be visible when user click on "change" button*/}
+      <label className="dashboard-username-pwrd font-semibold flex flex-col">
+        Current password:{' '}
+        <input
+          className="p-2 border"
+          placeholder="Provide your current password"
+          disabled
+        />
+      </label>
+      <div>
+        <span>change your username </span>
+        <button className="bg-orange-500 py-2 px-4 text-white rounded-md">
+          change
+        </button>
+      </div>
+      <label className="dashboard-email font-semibold text-lg flex flex-col pb-4">
+        Email: <input className="p-2 border" value={user?.email} disabled />
+      </label>
+
+      {/* TODO: this password will be visible when user click on "change" button*/}
+      <label className="dashboard-username-pwrd font-semibold flex flex-col">
+        Current password:{' '}
+        <input
+          className="p-2 border"
+          placeholder="Provide your current password"
+          disabled
+        />
+      </label>
+      <div>
+        <span>change your email </span>
+        <button className="bg-orange-500 py-2 px-4 text-white rounded-md">
+          change
+        </button>
+      </div>
+      <p className="pt-10">your email is not verified</p>
+    </div>
   );
 };
 
