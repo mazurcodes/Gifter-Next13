8a77b9e191606e56305c3da2dd185ac8413f3a4f
Bartlomiej <trafficap@gmail.com>
2023-05-05 20:18:57 +0200

    refactor dashboard/delete page

--- app/dashboard/delete/page.tsx
+++ app/dashboard/delete/page.tsx
@@ -1,11 +1,23 @@
 'use client';
 
+import { useDeleteCurrentUser } from '@/firebase/authUtils';
 import { auth } from '@/firebase/clientApp';
 import { extractErrorMessage } from '@/utils/server';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+
+type FormDataFields = {
+  password: string;
+};
 
 const DashboardDeletePage = () => {
   const [user, loading, error] = useAuthState(auth);
+  const { register, handleSubmit } = useForm<FormDataFields>();
+  const [deleteUser, isDeleted, working, errorDelete] = useDeleteCurrentUser();
+
+  const onSubmit = (formData: FormDataFields) => {
+    deleteUser(formData.password);
+  };
 
   if (loading) {
     return (
@@ -25,13 +37,34 @@ const DashboardDeletePage = () => {
 
   if (user)
     return (
-      <div className="dashboard-wrapper">
-        <p>We are sorry to see you go.</p>
+      <form className="dashboard-delete-form" onSubmit={handleSubmit(onSubmit)}>
+        <label className="dashboard-username-pwrd font-semibold flex flex-col pb-5">
+          Confirm your password:{' '}
+          <input
+            id="newPassword"
+            className="p-2 border"
+            type="password"
+            minLength={6}
+            {...register('password', { required: true })}
+          />
+        </label>
+
         <button className="bg-orange-500 py-2 px-4 text-white rounded-md ">
           Nevermind
         </button>
-        <button className="border py-2 px-4 rounded-md ml-4">Delete</button>
-      </div>
+        <button
+          type="submit"
+          className="border py-2 px-4 text-red-600 rounded-md ml-2"
+        >
+          {working ? 'Deleting' : 'Delete'}
+        </button>
+        {isDeleted && <p>User successfully deleted</p>}
+        {errorDelete && (
+          <p>
+            There was some error: {extractErrorMessage(errorDelete.message)}
+          </p>
+        )}
+      </form>
     );
 
   return (
