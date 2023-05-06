95ae407d2ba4cfa6f6b34e69e46dc336e00514d3
Bartlomiej <trafficap@gmail.com>
2023-05-06 20:19:38 +0200

    DashboardMenu with links with activeClass

--- app/components/Dashboard/DashboardMenu/index.tsx
+++ app/components/Dashboard/DashboardMenu/index.tsx
@@ -1,37 +1,51 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const DashboardMenu = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="dashboard-nav w-60">
       <ul className="flex flex-col gap-3 items-start">
         <Link
           href={'/dashboard'}
-          className="py-2 px-6 rounded-lg hover:bg-gray-50"
+          className={`${
+            pathname === '/dashboard' && 'font-bold'
+          } py-2 px-6 rounded-lg hover:bg-gray-50`}
         >
           General
         </Link>
         <Link
           href={'/dashboard/password'}
-          className="py-2 px-6 rounded-lg hover:bg-gray-50"
+          className={`${
+            pathname === '/dashboard/password' && 'font-bold'
+          } py-2 px-6 rounded-lg hover:bg-gray-50`}
         >
           Password
         </Link>
         <Link
           href={'/dashboard/wishlist'}
-          className="py-2 px-6 rounded-lg hover:bg-gray-50"
+          className={`${
+            pathname === '/dashboard/wishlist' && 'font-bold'
+          } py-2 px-6 rounded-lg hover:bg-gray-50`}
         >
           Wishlist
         </Link>
         <Link
           href={'/dashboard/summary'}
-          className="py-2 px-6 rounded-lg hover:bg-gray-50"
+          className={`${
+            pathname === '/dashboard/summary' && 'font-bold'
+          } py-2 px-6 rounded-lg hover:bg-gray-50`}
         >
           Statistics
         </Link>
         <div className="section-divider border-t border-gray-200 w-36 ml-6"></div>
         <Link
           href={'/dashboard/delete'}
-          className="py-2 px-6 text-red-600 rounded-lg hover:bg-gray-50"
+          className={`${
+            pathname === '/dashboard/delete' && 'font-bold'
+          }  text-red-600 py-2 px-6 rounded-lg hover:bg-gray-50`}
         >
           Delete account
         </Link>
