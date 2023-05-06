018b6db22009be9294c499bf9dbb4a9a8012ee1e
Bartlomiej <trafficap@gmail.com>
2023-05-06 14:16:18 +0200

    /dashboard user can change his or hers email address

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,31 @@
 'use client';
 
+import { useChangeEmail } from '@/firebase/authUtils';
 import { auth } from '@/firebase/clientApp';
 import { extractErrorMessage } from '@/utils/server';
+import { useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+
+type FormDataValues = {
+  email: string;
+  password: string;
+};
 
 const DashboardPage = () => {
   const [user, loading, error] = useAuthState(auth);
+  const { register, handleSubmit, watch, setValue } = useForm<FormDataValues>();
+  const [changeEmail, , working, errorEmail] = useChangeEmail();
+
+  const watchEmail = watch('email');
+
+  useEffect(() => {
+    user?.email && setValue('email', user.email);
+  }, [user, setValue]);
+
+  const onSubmit = (formData: FormDataValues) => {
+    changeEmail(formData.email, formData.password);
+  };
 
   if (loading) {
     return (
@@ -25,48 +45,39 @@ const DashboardPage = () => {
 
   if (user) {
     return (
-      <div className="dashboard-wrapper">
-        <label className="dashboard-username font-semibold text-lg flex flex-col pb-4">
-          Username: <input className="p-2 border" value="MazurCodes" disabled />
-        </label>
-
-        {/* TODO: this password will be visible when user click on "change" button*/}
-        <label className="dashboard-username-pwrd font-semibold flex flex-col">
-          Current password:{' '}
+      <form className="dashboard-email" onSubmit={handleSubmit(onSubmit)}>
+        <label className="dashboard-email font-semibold text-lg flex flex-col pb-10">
+          Email{' '}
           <input
-            className="p-2 border"
-            placeholder="Provide your current password"
-            disabled
+            className="p-2 border rounded-md font-normal text-sm mt-3"
+            type="email"
+            {...register('email')}
           />
         </label>
-        <div>
-          <span>change your username </span>
-          <button className="bg-orange-500 py-2 px-4 text-white rounded-md">
-            change
-          </button>
-        </div>
-        <label className="dashboard-email font-semibold text-lg flex flex-col pb-4">
-          Email:{' '}
-          <input className="p-2 border" value={user.email || ''} disabled />
-        </label>
 
-        {/* TODO: this password will be visible when user click on "change" button*/}
-        <label className="dashboard-username-pwrd font-semibold flex flex-col">
-          Current password:{' '}
-          <input
-            className="p-2 border"
-            placeholder="Provide your current password"
-            disabled
-          />
-        </label>
+        {watchEmail && watchEmail !== user.email && (
+          <label className="dashboard-email-pwrd font-semibold text-lg flex flex-col pb-10">
+            Password to change your email{' '}
+            <input
+              className="p-2 border rounded-md font-normal text-sm mt-3"
+              type="password"
+              {...register('password')}
+            />
+          </label>
+        )}
         <div>
-          <span>change your email </span>
-          <button className="bg-orange-500 py-2 px-4 text-white rounded-md">
-            change
+          <button
+            className="bg-orange-500 py-2 px-4 text-white rounded-md"
+            type="submit"
+          >
+            {working ? 'Saving' : 'Save Changes'}
           </button>
         </div>
-        <p className="pt-10">your email is not verified</p>
-      </div>
+        {errorEmail && (
+          <p>There was some error: {extractErrorMessage(errorEmail.message)}</p>
+        )}
+        <p className="pt-10">your email is not verified.</p>
+      </form>
     );
   }
 
