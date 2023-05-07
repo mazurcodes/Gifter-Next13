f9631132b80faabab55b5c59c91cffd1486d0957
Bartlomiej <trafficap@gmail.com>
2023-05-07 13:40:00 +0200

    DropdownIcon for DashboardMobileMenuBtn

--- app/components/Dashboard/DashboardMobileMenuBtn/index.tsx
+++ app/components/Dashboard/DashboardMobileMenuBtn/index.tsx
@@ -1,8 +1,10 @@
 'use client';
 import { useTriggerOnBlur } from '@/utils/client';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { useRef, useState } from 'react';
-import DashboardMobileMenu from '../DashboardMobileMenu';
+import DashboardMobileMenu from '@/components/Dashboard/DashboardMobileMenu';
+import DropdownIcon from '@/assets/DropdownIcon.svg';
 
 const DashboardMobileMenuBtn = () => {
   const pathname = usePathname();
@@ -25,9 +27,15 @@ const DashboardMobileMenuBtn = () => {
     <div
       ref={wrapperRef}
       onClick={handleClick}
-      className="p-2 px-3 border-2 border-orange-500 rounded-md relative hidden sm:block"
+      className="p-2 px-4 border-2 border-orange-500 rounded-md relative hidden sm:block"
     >
       <p>{currentLinkName}</p>
+      <Image
+        src={DropdownIcon}
+        alt="dropdown icon"
+        className={`absolute top-2 right-3 ${isOpen && 'rotate-180'}`}
+        width={26}
+      />
       {isOpen && <DashboardMobileMenu />}
     </div>
   );
